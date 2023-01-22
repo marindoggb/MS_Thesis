cd575497d427cae87a921c1919b6918351a28f34
marindoggb <48791147+marindoggb@users.noreply.github.com>
2023-01-22 16:44:45 +0000

    Update Impute_a_WD.R

--- Impute_a_WD.R
+++ Impute_a_WD.R
@@ -15,37 +15,15 @@ imp_PMM_m25_WD <- mice(gognmNA,method="pmm",predictorMatrix = predM ,m=25, seed 
 save(imp_PMM_m25_WD, file="imp_PMM_m25_WD")
 
 
-## MICE
+## MI-MICE
 
 imp_MICE_m25_WD <- mice(gognmNA,m=25,predictorMatrix = predM,method=meth, seed = 71332, print = FALSE,maxit=10) 
 save(imp_MICE_m25_WD, file="imp_MICE_m25_WD")
 
 
-## SI
+## SI-PMM
 
 imp_SI_m25_WD <- mice(gognmNA,method="pmm",predictorMatrix = predM,m=1, seed = 71332, print = FALSE,maxit=10) 
 save(imp_SI_m25_WD, file="imp_SI_m25_WD")
-
-
-
-
-
-
-
-
-
-buh<-gognmNA
-colnames(buh)<-c("pin", "Age", "Nordic", "Residence", "Cohabitation", "Education", "Income", "Working_status", "Work_schedule", "Deprivation", "BMI", "Num_Children", "Smoking", "Binge_drinking", "Worst_trauma", "GAD7", "LEC", "PHQ9", "PHQ15", "PMS", "PSQIA", "PCL", "PSQI", "ACE","Coping_Ability")
-
-imp<-mice(buh, m=1, maxit=0)
-predM<-imp$predictorMatrix
-predM[c(1:25), 1]<-0
-
-
-imp_PMM_m25_WDbluu <- mice(buh,method="pmm",predictorMatrix = predM ,m=25, seed = 71332, print = FALSE,maxit=10) # donors eru hversu stort mengi i kring er buid til til ad velja observed value
-
-
-save(imp_PMM_m25_WDbluu, file="imp_PMM_m25_WDbluu")
-
 
 
